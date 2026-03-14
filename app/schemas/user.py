939dbe11f51e185f87c647134cfbed43160b1dab
Dylan <dylanmckay8939@gmail.com>
2026-03-14 00:49:49 -0400

    Add user schema

---
+++ app/schemas/user.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+    
+    class Config:
+        from_attributes = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    email: str | None = None
